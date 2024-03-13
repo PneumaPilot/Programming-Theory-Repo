a7bf9c845e18042659d1a0a05b47614f5024e7c9
PneumaPilot <pneumapilot@gmail.com>
2024-03-12 22:05:01 -0400

    starting denominations
    - baptist has been prototyped
    - other denomination scripts have been created

---
+++ Programming Theory Project/Assets/Scripts/Baptist.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// INHERITANCE
+public class Baptist : Church
+{
+    public Baptist() { }
+
+    public Baptist(int members)
+    {
+        this.m_members = members;
+        this.m_treasury = 0;
+    }
+
+    public override void Pray()
+    {
+        // Print Baptist prayer
+    }
+
+    public override void Preach()
+    {
+        // Describe Baptist sermon
+    }
+
+    public override void Baptize(int numberOfConverts)
+    {
+        // Describe baptism and add members
+    }
+
+    public override void Communion()
+    {
+        // Describer Baptist communion
+    }
+
+    public override void Sing()
+    {
+        // Describe example song
+    }
+}
--- Programming Theory Project/Assets/Scripts/Church.cs
+++ Programming Theory Project/Assets/Scripts/Church.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Church : MonoBehaviour
+public abstract class Church : MonoBehaviour
 {
-    private int m_members;
-    private float m_treasury;
+    protected int m_members;
+    protected float m_treasury;
+
+    public Church()
+    {
+        m_members = 0;
+        m_treasury = 0;
+    }
 
     public Church(int members)
     {
@@ -17,6 +23,7 @@ public class Church : MonoBehaviour
         {
             m_members = 0;
         }
+        m_treasury = 0;
     }
 
     // ENCAPSULATION
@@ -46,4 +53,23 @@ public class Church : MonoBehaviour
             return m_treasury;
         }
     }
+
+    // ABSTRACTION
+    public abstract void Pray(); // Print example prayer on screen
+
+    public virtual void TakeOffering(float offering) // Adds money to the treasury
+    {
+        if (offering > 0)
+        {
+            m_treasury += offering;
+        }
+    }
+
+    public abstract void Preach(); // Describe sermon on screen
+
+    public abstract void Baptize(int numberOfConverts); // Describe baptism on screen
+
+    public abstract void Sing(); // Describe sample song on screen
+
+    public abstract void Communion(); // Describe communion celebration on screen
 }
