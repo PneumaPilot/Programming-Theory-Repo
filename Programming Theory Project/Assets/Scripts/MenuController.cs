778acdda31d2d719e0ff35834473ae26c7ba4b2a
PneumaPilot <pneumapilot@gmail.com>
2024-03-13 12:04:58 -0400

    added button functionality
    - created method to handle which button is pressed

--- Programming Theory Project/Assets/Scripts/MenuController.cs
+++ Programming Theory Project/Assets/Scripts/MenuController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public Church ChosenChurch;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,26 @@ public class MenuController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SelectDenomination(int denomination)
+    {
+        switch (denomination){
+            case 0:
+                ChosenChurch = new Baptist();
+                break;
+            case 1:
+                ChosenChurch = new Presbyterian();
+                break;
+            case 2:
+                ChosenChurch = new Catholic();
+                break;
+            case 3:
+                ChosenChurch = new Charismatic();
+                break;
+        }
+        DontDestroyOnLoad(ChosenChurch);
+
+
     }
 }
